3c0f3391ff5cb57a702aa4a6a7441a6b264ecd6d
スシドン <111954145+AaiEa00@users.noreply.github.com>
2026-09-24 16:21:25 +0900

    feat: プレイヤーとCPUの選んだ手を画像で表示できるようにした
    * 画像のスプライトモードを変更
    
    * シーン名をわかりやすく変更した
    
    * シーン名をわかりやすく変更した
    
    * プレイヤーとCPUの選んだ手を画像で表示できるようになった

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    enum Hands
+    {
+        グー,
+        チョキ,
+        パー
+    }
+
+    static int handCount = System.Enum.GetValues(typeof(Hands)).Length;
+
+    Hands playerHand;
+    Hands cpuHand;
+
+    [SerializeField] GameObject[] hands = new GameObject[handCount];
+    [SerializeField] GameObject[] results = new GameObject[2];
+    [SerializeField] Sprite[] handSprites = new Sprite[handCount];
+
+    private void Awake()
+    {
+        foreach (var res in results)
+        {
+            res.SetActive(false);
+        }
+    }
+
+    public void OnClick(string hand)
+    {
+        playerHand = (Hands)System.Enum.Parse(typeof(Hands), hand);
+
+        cpuHand = (Hands)Random.Range(0, handCount);
+
+        Result(playerHand, cpuHand);
+    }
+
+    void Result(Hands player, Hands cpu)
+    {
+        // ボタンを押したときに手の画像を非表示にして、結果のUIを表示する
+        foreach (var hand in hands)
+        {
+            hand.gameObject.SetActive(false);
+        }
+        foreach (var res in results)
+        {
+            res.SetActive(true);
+        }
+
+        Image playerImage = results[0].GetComponentInChildren<Image>();
+        Image cpuImage = results[1].GetComponentInChildren<Image>();
+
+        TextMeshProUGUI playerText = playerImage.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI cpuText = cpuImage.transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+
+        playerImage.sprite = handSprites[(int)player];
+        cpuImage.sprite = handSprites[(int)cpu];
+
+        playerText.text = player.ToString();
+        cpuText.text = cpu.ToString();
+    }
+}
